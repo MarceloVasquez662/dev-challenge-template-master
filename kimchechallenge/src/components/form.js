bcba0ac30ea00d05b96bd8c18d3def2e58b97ab1
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-05-12 13:17:41 -0400

    Mejora de interfaz

--- kimchechallenge/src/components/form.js
+++ kimchechallenge/src/components/form.js
@@ -26,6 +26,14 @@ class Form extends Component {
         }
     }
 
+    porContinente = () => {
+
+    }
+
+    porIdioma = () => {
+
+    }
+
 
     render() {
         return (
@@ -35,18 +43,34 @@ class Form extends Component {
                         <input type="text" name="pais" id="pais" placeholder='Busca tu país aquí' className='form-control' onChange={this.buscarPais} />
                     </form>
                 </div>
-                <div className='agruparPor' id='agruparPor'>
-                    <h5>Agrupar por:</h5>
-                    <button className='btn btn-success m-3' >Por continente</button>
-                    <button className='btn btn-info m-1'>Por idioma</button>
-                </div>
-                <div className="paises row justify-content-center">
-                    {
-                        this.state.paises.map(pais => {
-                            return <CardPaises key={pais.name} nombrePais={pais.name} codigo={pais.code} emoji={pais.emoji} capital={pais.capital} moneda={pais.currency} />
-                        })
-                    }
-                </div>
+
+
+                {
+                    this.state.paises.length === 0 ?
+                        <div className='text-center'>
+                            <h1>
+                                <span className='emoji' role="img" aria-label='emoji'>
+                                    &#x1F972;
+                                </span></h1>
+                            <h2>No hay resultados para tu busqueda </h2>
+                        </div>
+                        :
+                        <div>
+                            <div className='agruparPor' id='agruparPor'>
+                                <h5>Agrupar por:</h5>
+                                <button className='btn btn-success m-3' onClick={this.porContinente}>Por continente</button>
+                                <button className='btn btn-info m-1' onClick={this.porIdioma}>Por idioma</button>
+                            </div>
+
+                            <div className="paises row justify-content-center">
+                                {this.state.paises.map(pais => {
+                                    return <CardPaises key={pais.name} nombrePais={pais.name} codigo={pais.code} emoji={pais.emoji} capital={pais.capital} moneda={pais.currency} />
+                                })}
+                            </div>
+
+                        </div>
+                }
+
             </ div>
         )
     }
